58b48031df89028c30506318bb8495d9c9621aaf
Dave Long <dlong@cagedata.com>
2016-09-15 16:28:42 -0400

    Fix errors when using Kubernetes

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM alpine:latest
 
-RUN apk update && apk add openssl openssl-dev gcc python python-dev py-pip musl-dev libffi-dev
+RUN apk --update add openssl openssl-dev gcc python python-dev py-pip musl-dev libffi-dev ca-certificates
 RUN pip install --quiet pyopenssl
 
 ADD https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-126.0.0-linux-x86_64.tar.gz /opt/google-cloud-sdk.tar.gz
 RUN tar -xzf /opt/google-cloud-sdk.tar.gz -C /opt && \
   /opt/google-cloud-sdk/install.sh --quiet --path-update=true --command-completion=true \
-    --usage-reporting=true --additional-components=kubectl
+    --usage-reporting=true --additional-components kubectl beta
 ENV PATH /opt/google-cloud-sdk/bin:$PATH
 
 COPY docker-entrypoint.sh /opt/google-cloud-sdk/docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,20 +8,22 @@ set -e
 GCLOUD_ROOT=/opt/google-cloud-sdk
 GCLOUD="${GCLOUD_ROOT}/bin/gcloud"
 
+if [ -n "${GCLOUD_ACCOUNT_FILE}" ]; then
+  ${GCLOUD} auth activate-service-account --keyfile="${GCLOUD_ACCOUNT_FILE}"
+fi
+
 if [ -n "${GCLOUD_ACCOUNT}" ]; then
-  echo ${GCLOUD_ACCOUNT} >> "${GCLOUD_ROOT}/auth.base64"
-  base64 -d "${GCLOUD_ROOT}/auth.base64" >> "${GCLOUD_ROOT}/auth.json"
+  export GCLOUD_ACCOUNT_FILE="${GCLOUD_ROOT}/auth.json"
+  echo "${GCLOUD_ACCOUNT}" | base64 -d > "${GCLOUD_ACCOUNT_FILE}"
   ${GCLOUD} auth activate-service-account \
     --key-file="${GCLOUD_ROOT}/auth.json" \
-    ${GCLOUD_ACCOUNT_EMAIL}
+    "${GCLOUD_ACCOUNT_EMAIL}"
 fi
 
-if [ -n "${GCLOUD_ACCOUNT_FILE}" ]; then
-  ${GCLOUD} auth activate-service-account --keyfile=${GCLOUD_ACCOUNT_FILE}
-fi
+export GOOGLE_APPLICATION_CREDENTIALS="${GCLOUD_ACCOUNT_FILE}"
 
 if [ -n "${KUBE_CLUSTER}" ]; then
-  ${GCLOUD} container clusters get-credentials ${KUBE_CLUSTER}
+  ${GCLOUD} container clusters get-credentials "${KUBE_CLUSTER}"
 fi
 
 exec "$@"
